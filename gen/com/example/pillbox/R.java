2018a4ffff9a9c0fc60eadacd7926d3782730d5d
LEV LEETIAN <nleetian@gmail.com>
2015-05-30 15:14:05 +0800

    Added today week month buttons

--- gen/com/example/pillbox/R.java
+++ gen/com/example/pillbox/R.java
@@ -1123,16 +1123,19 @@ containing a value of this type.
         public static final int list_selector=0x7f02006c;
         public static final int logo=0x7f02006d;
         public static final int medicine=0x7f02006e;
-        public static final int mycube=0x7f02006f;
-        public static final int profile=0x7f020070;
-        public static final int register_idle_state=0x7f020071;
-        public static final int register_pressed_state=0x7f020072;
-        public static final int register_selector=0x7f020073;
-        public static final int sign_in_idle_state=0x7f020074;
-        public static final int sign_in_pressed_state=0x7f020075;
-        public static final int sign_in_selector=0x7f020076;
-        public static final int star_idle_state=0x7f020077;
-        public static final int star_pressed_state=0x7f020078;
+        public static final int monthly_selector=0x7f02006f;
+        public static final int mycube=0x7f020070;
+        public static final int profile=0x7f020071;
+        public static final int register_idle_state=0x7f020072;
+        public static final int register_pressed_state=0x7f020073;
+        public static final int register_selector=0x7f020074;
+        public static final int sign_in_idle_state=0x7f020075;
+        public static final int sign_in_pressed_state=0x7f020076;
+        public static final int sign_in_selector=0x7f020077;
+        public static final int star_idle_state=0x7f020078;
+        public static final int star_pressed_state=0x7f020079;
+        public static final int today_selector=0x7f02007a;
+        public static final int weekly_selector=0x7f02007b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
